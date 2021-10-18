bf375879689d9ea57f73228e3409ecdd0e79f08f
HaPuLo <kimquanghoang94@gmail.com>
2021-10-18 17:20:48 +0700

    add in nickpallet

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -237,11 +237,15 @@ impl pallet_timestamp::Config for Runtime {
 	type WeightInfo = ();
 }
 
+// The previously defined parameter_type is used as a configuration parameter.
 parameter_types! {
+	// The u128 constant value 500 is aliased to a type named ExistentialDeposit.
 	pub const ExistentialDeposit: u128 = 500;
+	// A heuristic that is used for weight estimation.
 	pub const MaxLocks: u32 = 50;
 }
 
+// The previously defined parameter_type is used as a configuration parameter.
 impl pallet_balances::Config for Runtime {
 	type MaxLocks = MaxLocks;
 	type MaxReserves = ();
@@ -277,6 +281,44 @@ impl pallet_template::Config for Runtime {
 	type Event = Event;
 }
 
+
+
+// Add this code block to your template for Nicks:
+parameter_types! {
+    // Choose a fee that incentivizes desireable behavior.
+    pub const NickReservationFee: u128 = 100;
+    pub const MinNickLength: u32 = 8;
+    // Maximum bounds on storage are important to secure your chain.
+    pub const MaxNickLength: u32 = 32;
+}
+
+impl pallet_nicks::Config for Runtime {
+    // The Balances pallet implements the ReservableCurrency trait.
+    // `Balances` is defined in `construct_runtimes!` macro. See below.
+    // https://substrate.dev/rustdocs/latest/pallet_balances/index.html#implementations-2
+    type Currency = Balances;
+
+    // Use the NickReservationFee from the parameter_types block.
+    type ReservationFee = NickReservationFee;
+
+    // No action is taken when deposits are forfeited.
+    type Slashed = ();
+
+    // Configure the FRAME System Root origin as the Nick pallet admin.
+    // https://substrate.dev/rustdocs/latest/frame_system/enum.RawOrigin.html#variant.Root
+    type ForceOrigin = frame_system::EnsureRoot<AccountId>;
+
+    // Use the MinNickLength from the parameter_types block.
+    type MinLength = MinNickLength;
+
+    // Use the MaxNickLength from the parameter_types block.
+    type MaxLength = MaxNickLength;
+
+    // The ubiquitous event type.
+    type Event = Event;
+}
+
+
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
 	pub enum Runtime where
@@ -294,6 +336,9 @@ construct_runtime!(
 		Sudo: pallet_sudo::{Pallet, Call, Config<T>, Storage, Event<T>},
 		// Include the custom logic from the pallet-template in the runtime.
 		TemplateModule: pallet_template::{Pallet, Call, Storage, Event<T>},
+		//Add in Nicks palllet cofiguration
+		Nicks: pallet_nicks::{Pallet, Call, Storage, Event<T>},
+		
 	}
 );
 
